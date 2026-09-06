859f4ad172715a2bc7077dc4646735bca91535ef
Guy Torbet <guy@torbet.co>
2026-09-06 12:34:41 +0100

    cleanup install script

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ ROOT="$(cd "$(dirname "$0")" && pwd)"
 
 mkdir -p "$HOME/.config"
 
-ln -s "$ROOT/.config/ghostty"       "$HOME/.config/ghostty"
-ln -s "$ROOT/.config/nvim"          "$HOME/.config/nvim"
-ln -s "$ROOT/.config/tmux"          "$HOME/.config/tmux"
-ln -s "$ROOT/.config/starship.toml" "$HOME/.config/starship.toml"
-ln -s "$ROOT/.zshrc"                "$HOME/.zshrc"
+for item in ghostty nvim tmux starship.toml; do
+  ln -sfn "$ROOT/.config/$item" "$HOME/.config/$item"
+done
+
+ln -sfn "$ROOT/.zshrc" "$HOME/.zshrc"
